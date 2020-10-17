0122f4e1ec13be8aaa25158c4b91a5a487cd57c8
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:12:07 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan7/integer/IF110119028Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan7.integer;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk
+ */
+public class IF110119028Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
